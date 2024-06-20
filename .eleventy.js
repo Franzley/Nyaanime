d7d9363323ff4a9bd945f711843f09775328e385
franzley <franzleybonhomme003@gmail.com>
2024-06-20 13:38:23 -0400

    Created component to automatically generate lists, edited list in header, updated responsive image tags

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,9 @@ module.exports = function(eleventyConfig) {
 
   // Add files to public folder
   eleventyConfig.addPassthroughCopy('./src/styles');
+  eleventyConfig.addPassthroughCopy('./src/assets');
   eleventyConfig.addPassthroughCopy('./src/images');
+
 
   return {
     dir: {
