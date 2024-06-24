90f323b368b2df4f422272269bb9543f44d1df06
franzley <franzleybonhomme003@gmail.com>
2024-06-24 11:14:27 -0400

    moved images into assets

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,6 @@ module.exports = function(eleventyConfig) {
   // Add files to public folder
   eleventyConfig.addPassthroughCopy('./src/styles');
   eleventyConfig.addPassthroughCopy('./src/assets');
-  eleventyConfig.addPassthroughCopy('./src/images');
 
 
   return {
