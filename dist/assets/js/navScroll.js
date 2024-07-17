436e8a5fa9853b2c68dc834aba9188960bcd74be
franzley <franzleybonhomme003@gmail.com>
2024-07-17 04:06:34 -0400

    removed dist

--- dist/assets/js/navScroll.js
+++
@@ -1,5 +0,0 @@
-const navbar = document.querySelector("header");
-window.addEventListener("scroll", () => {
-  // When the page is scrolling, apply class to nav
-  navbar.classList.toggle("window-scroll", window.scrollY > 0);
-});
