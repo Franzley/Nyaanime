71faf4888d7c51ed0d92c1f20c36ba2ebd8c6c71
franzley <franzleybonhomme003@gmail.com>
2024-07-18 13:25:08 -0400

    edited navscroll to function based on mobile or desktop, edited css for scroll functionality

--- src/assets/js/navScroll.js
+++ src/assets/js/navScroll.js
@@ -1,5 +1,9 @@
 const navbar = document.querySelector("header");
 window.addEventListener("scroll", () => {
   // When the page is scrolling, apply class to nav
-  navbar.classList.toggle("window-scroll", window.scrollY > 0);
+  if(window.innerWidth < 940){
+    document.body.classList.toggle("window-scroll", window.scrollY > 0);
+  }else{
+    document.body.classList.toggle("window-scroll", window.scrollY > document.getElementById("navigation").offsetHeight);
+  }
 });
